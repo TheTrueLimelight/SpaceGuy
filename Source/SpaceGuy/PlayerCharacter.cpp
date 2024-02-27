cd6b672c73e01159db86bd266a0849f98cf4982c
TheTrueLimelight <109849209+TheTrueLimelight@users.noreply.github.com>
2024-02-27 01:13:53 +0000

    Made a shitty movement system

--- Source/SpaceGuy/PlayerCharacter.cpp
+++ Source/SpaceGuy/PlayerCharacter.cpp
@@ -10,15 +10,65 @@ APlayerCharacter::APlayerCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	//Component creation
-	CameraMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CameraMesh"));
-	View = CreateDefaultSubobject<UCameraComponent>(TEXT("View"));
+	CameraMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CameraMesh")); //CharacterMesh defintion
+	View = CreateDefaultSubobject<UCameraComponent>(TEXT("View"));   //CharacterMesh defintion
+
+	//Character settings creation
+	MovementSpeed = 15.0f; //The Speed of the character
+	CurrentPerspective = 0; //Which perspective the character is in (0 First person 1 third person)
+	RotationSpeed = 1.0f;
 
 	//Setup camera
-	View->FieldOfView = 100.0f;
-	View->SetupAttachment(CameraMesh);
-	View->SetRelativeLocation(FVector(-100.0f, 0.0f, 50.0f));
+	View->FieldOfView = 100.0f; //Sets FOV to 100
+	View->SetupAttachment(CameraMesh); //Attaches the Camera to the CharacterMesh
+	View->SetRelativeLocation(FVector(70.0f, 0.0f, 50.0f)); //Sets the relative location in comparison to CharacterMesh
 
+	//Checks
+	IsJumping = false;
 }
+
+void APlayerCharacter::MoveLR(float movementDelta) {
+	FVector NewPos = GetActorLocation(); //Defines a Vector with the current location
+	NewPos.Y += movementDelta*MovementSpeed; //Alters coordinates on the Y axis
+	SetActorLocation(NewPos); //Sets the actor location with the altered coordinates
+}
+
+void APlayerCharacter::MoveWS(float movementDelta) {
+	FVector NewPos = GetActorLocation(); //Defines a Vector with the current location
+	NewPos.X += movementDelta*MovementSpeed; //Alters coordinates on the Y axis
+	SetActorLocation(NewPos); //Sets the actor location with the altered coordinates
+}
+
+void APlayerCharacter::SetView(int view) {  //Sets the character view based on the string given to it
+	if (view == 0) { View->SetRelativeLocation(FVector(150.0f, 0.0f, 50.0f)); } //If 0 Set to first person
+	else if (view == 1) { View->SetRelativeLocation(FVector(-150.0f, 0.0f, 50.0f)); } //If 1 Set to third person
+};
+
+void APlayerCharacter::SwitchView(){  //Cycles the view through all the different perspectives
+	CurrentPerspective += 1; //Adds 1 to the Current Prespecitive
+	CurrentPerspective %= 2; //Mods it by the amount of persectives there are
+	SetView(CurrentPerspective); //Sets the view to the current prespective
+};
+
+void APlayerCharacter::Pitch(float rotationDelta) { //X Axis Rotatio
+	auto Rotation = GetActorRotation();
+	Rotation.Pitch += rotationDelta * RotationSpeed;
+	SetActorRotation(Rotation);
+}
+void APlayerCharacter::Yaw(float rotationDelta) { //Y Axis Rotation
+	auto Rotation = GetActorRotation();
+	Rotation.Yaw += rotationDelta * RotationSpeed;
+	SetActorRotation(Rotation);
+}
+
+void APlayerCharacter::Roll(float rotationDelta) { //Z Axis Rotation
+	auto Rotation = GetActorRotation();
+	Rotation.Roll += rotationDelta * RotationSpeed;
+	SetActorRotation(Rotation);
+}
+
+
+
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
@@ -38,6 +88,13 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveLR"), this, &APlayerCharacter::MoveLR);
+	PlayerInputComponent->BindAxis(TEXT("MoveWS"), this, &APlayerCharacter::MoveWS);
 	
+	PlayerInputComponent->BindAxis(TEXT("Yaw"), this, &APlayerCharacter::Yaw);
+	PlayerInputComponent->BindAxis(TEXT("Pitch"), this, &APlayerCharacter::Pitch);
+	PlayerInputComponent->BindAxis(TEXT("Roll"), this, &APlayerCharacter::Roll);
+
 }
 
--- Source/SpaceGuy/PlayerCharacter.h
+++ Source/SpaceGuy/PlayerCharacter.h
@@ -14,14 +14,35 @@ class SPACEGUY_API APlayerCharacter : public APawn
 public:
 	// Sets default values for this pawn's properties
 	APlayerCharacter();
+	//Delta
+	
+	//Defines possible ways to move character
+	void MoveLR(float movementDelta);//Method for Moving Character left or right
+	void MoveWS(float movementDelta);//Method for Moving Character forward or backward
+	void Jump(float movementDelta);//Method for Moving Character up
+	void CheckJump();              //Check if player is jumping or not
+
+	//Defines possible ways to move camera
+	void SetView(int view);      //Sets the character view based on the string given to it
+	void SwitchView();           //Cycles the view through all the different perspectives
+	
+	void Pitch(float rotationDelta); //X Axis Rotation
+	void Yaw(float rotationDelta); //Y Axis Rotation
+	void Roll(float rotationDelta); //Z Axis Rotation
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	//Pawn Camera
-	UCameraComponent* View;
-	UStaticMeshComponent* CameraMesh;
+	UPROPERTY(EditAnywhere) UCameraComponent* View; //CharacterMesh defintion
+	UPROPERTY(EditAnywhere)UStaticMeshComponent* CameraMesh; //CharacterMesh defintion
+	UPROPERTY(EditAnywhere, Category = "Character Settings") float MovementSpeed; //The Speed of the character movement
+	UPROPERTY(EditAnywhere, Category = "Character Settings") float RotationSpeed; //The Speed of the character rotation
+	UPROPERTY(EditAnywhere, Category = "Character Settings") int CurrentPerspective; //Which perspective the character is in (0 First person 1 third person)
+	UPROPERTY() bool IsJumping; //If the character is jumping or not
+
+
 
 public:	
 	// Called every frame
