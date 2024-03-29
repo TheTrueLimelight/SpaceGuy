fb4ebf396d2cae179bc8bb6b34bb717768023f17
TheTrueLimelight <109849209+TheTrueLimelight@users.noreply.github.com>
2024-03-29 18:42:16 +0000

    Pawn -> Character
    Changed from pawn to character
    Need to convert more class methods to use ue5 methods rather than do it manually

--- Source/SpaceGuy/GameCharacter.cpp
+++ Source/SpaceGuy/GameCharacter.cpp
@@ -3,19 +3,72 @@
 
 #include "GameCharacter.h"
 
-// Sets default values
 AGameCharacter::AGameCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Component creation
+	CameraMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CameraMesh")); //CharacterMesh defintion
+	View = CreateDefaultSubobject<UCameraComponent>(TEXT("View"));   //CharacterMesh defintion
+
+	//Character settings creation
+	MovementSpeed = 15.0f; //The Speed of the character
+	CurrentPerspective = 0; //Which perspective the character is in (0 First person 1 third person)
+	RotationSpeed = 1.0f;
+
+	//Setup camera
+	CameraMesh->SetupAttachment(GetCapsuleComponent());
+	View->FieldOfView = 100.0f; //Sets FOV to 100
+	View->SetupAttachment(GetCapsuleComponent()); //Attaches the Camera to the CharacterMesh
+	View->SetRelativeLocation(FVector(150.0f, 0.0f, 50.0f)); //Sets the relative location in comparison to CharacterMesh
+
+	//Checks
+	IsJumping = false;
 }
+
+void AGameCharacter::MoveLR(float movementDelta) {
+	AddMovementInput(GetActorRightVector(), movementDelta * MovementSpeed);
+}
+
+void AGameCharacter::MoveWS(float movementDelta) {
+	AddMovementInput(GetActorForwardVector(), movementDelta*MovementSpeed);
+}
+
+void AGameCharacter::SetView(int view) {  //Sets the character view based on the string given to it
+	if (view == 0) { View->SetRelativeLocation(FVector(150.0f, 0.0f, 50.0f)); } //If 0 Set to first person
+	else if (view == 1) { View->SetRelativeLocation(FVector(-150.0f, 0.0f, 50.0f)); } //If 1 Set to third person
+};
+
+void AGameCharacter::SwitchView() {  //Cycles the view through all the different perspectives
+	CurrentPerspective += 1; //Adds 1 to the Current Prespecitive
+	CurrentPerspective %= 2; //Mods it by the amount of persectives there are
+	SetView(CurrentPerspective); //Sets the view to the current prespective
+};
+
+void AGameCharacter::Pitch(float rotationDelta) { //X Axis Rotatio
+	auto Rotation = GetActorRotation();
+	Rotation.Pitch += rotationDelta * RotationSpeed;
+	SetActorRotation(Rotation);
+}
+void AGameCharacter::Yaw(float rotationDelta) { //Y Axis Rotation
+	AddControllerYawInput(rotationDelta * RotationSpeed);
+}
+
+void AGameCharacter::Roll(float rotationDelta) { //Z Axis Rotation
+	auto Rotation = GetActorRotation();
+	Rotation.Roll += rotationDelta * RotationSpeed;
+	SetActorRotation(Rotation);
+}
+
+
+
 
 // Called when the game starts or when spawned
 void AGameCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -30,5 +83,13 @@ void AGameCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-}
+	PlayerInputComponent->BindAxis(TEXT("MoveLR"), this, &AGameCharacter::MoveLR);
+	PlayerInputComponent->BindAxis(TEXT("MoveWS"), this, &AGameCharacter::MoveWS);
 
+	PlayerInputComponent->BindAxis(TEXT("Yaw"), this, &AGameCharacter::Yaw);
+	PlayerInputComponent->BindAxis(TEXT("Pitch"), this, &AGameCharacter::Pitch);
+	PlayerInputComponent->BindAxis(TEXT("Roll"), this, &AGameCharacter::Roll);
+
+
+
+}
--- Source/SpaceGuy/GameCharacter.h
+++ Source/SpaceGuy/GameCharacter.h
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
+
+#include "Camera/CameraComponent.h"
+#include "Engine/StaticMesh.h"
+
+#include "Components/CapsuleComponent.h"
 #include "GameFramework/Character.h"
 #include "GameCharacter.generated.h"
+
 
 UCLASS()
 class SPACEGUY_API AGameCharacter : public ACharacter
@@ -15,9 +20,28 @@ public:
 	// Sets default values for this character's properties
 	AGameCharacter();
 
+	//Defines possible ways to move character
+	void MoveLR(float movementDelta);//Method for Moving Character left or right
+	void MoveWS(float movementDelta);//Method for Moving Character forward or backward
+
+	//Defines possible ways to move camera
+	void SetView(int view);      //Sets the character view based on the string given to it
+	void SwitchView();           //Cycles the view through all the different perspectives
+
+	void Pitch(float rotationDelta); //X Axis Rotation
+	void Yaw(float rotationDelta); //Y Axis Rotation
+	void Roll(float rotationDelta); //Z Axis Rotation
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere) UCameraComponent* View; //CharacterMesh defintion
+	UPROPERTY(EditAnywhere)UStaticMeshComponent* CameraMesh; //CharacterMesh defintion
+	UPROPERTY(EditAnywhere, Category = "Character Settings") float MovementSpeed; //The Speed of the character movement
+	UPROPERTY(EditAnywhere, Category = "Character Settings") float RotationSpeed; //The Speed of the character rotation
+	UPROPERTY(EditAnywhere, Category = "Character Settings") int CurrentPerspective; //Which perspective the character is in (0 First person 1 third person)
+	UPROPERTY() bool IsJumping; //If the character is jumping or not
 
 public:	
 	// Called every frame
